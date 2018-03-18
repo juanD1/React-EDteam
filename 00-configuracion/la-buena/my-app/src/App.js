2344d9336dc523c60bf9a2de9e61775721e9ef25
JuanD1 <juandacor@outlook.com>
2018-03-18 18:38:24 -0500

    probando react-bootstrap

--- 00-configuracion/la-buena/my-app/src/App.js
+++ 00-configuracion/la-buena/my-app/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {Button} from 'react-bootstrap'
 import logo from './logo.svg';
 import './App.css';
 
@@ -12,7 +13,8 @@ class App extends Component {
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+				</p>
+					<Button>default</Button>
       </div>
     );
   }
