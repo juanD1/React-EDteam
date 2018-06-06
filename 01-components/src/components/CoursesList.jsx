1ca9b25c1ac9ce5c015c4eac55278da76636fe92
JuanD1 <juandacor@outlook.com>
2018-06-05 22:38:45 -0500

    /

--- 01-components/src/components/CoursesList.jsx
+++ 01-components/src/components/CoursesList.jsx
@@ -3,17 +3,17 @@ import Course from './Course'
 
 const CoursesList = (props) => (   
   <ul style={{textAlign: 'left'}}>
-       {
-        props.courses.map(course => (
-          <Course 
-          key={course.id}
-          id={course.id}
-          name={course.name}
-          teacher={course.teacher}
-          />   
-        ))            
+      {
+       props.courses.map(course => (
+         <Course 
+         key={course.id}
+         id={course.id}
+         name={course.name}
+         teacher={course.teacher}
+         />   
+       ))            
       }
-    </ul>                     
+  </ul>                     
 )
     
 
