b1019f740cb2a312ffb9ac08565a791971664023
JuanD1 <juandacor@outlook.com>
2018-06-11 18:46:07 -0500

    sitio autenticado

--- 02-routes/src/index.js
+++ 02-routes/src/index.js
@@ -2,11 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './components/App';
-import StaticSite from './routes/Router'
+// import StaticSite from './routes/Router'
+import AuthSite from './routes/Auth'
 import registerServiceWorker from './registerServiceWorker';
 
 // ReactDOM.render(<App />, document.getElementById('root'));
 // registerServiceWorker();
 
-ReactDOM.render(<StaticSite />, document.getElementById('root'));
+// ReactDOM.render(<StaticSite />, document.getElementById('root'));
+// registerServiceWorker();
+
+ReactDOM.render(<AuthSite />, document.getElementById('root'));
 registerServiceWorker();
