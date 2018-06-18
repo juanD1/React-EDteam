56b470d35c93885f7e9e75fb5026c27d99d64968
JuanD1 <juandacor@outlook.com>
2018-06-18 14:42:39 -0500

    prettier

--- 03-autenticacion-firebase/src/pages/Login.jsx
+++ 03-autenticacion-firebase/src/pages/Login.jsx
@@ -4,38 +4,38 @@ import { login } from '.././helpers/Auth'
 class Login extends Component {
 	constructor(props) {
 		super(props)
-	
+
 		this.state = { loginMessage: null }
 
-		this.handleOnSubmit = this.handleOnSubmit.bind(this)		
+		this.handleOnSubmit = this.handleOnSubmit.bind(this)
 	}
 
 	handleOnSubmit(e) {
 		e.preventDefault()
 		alert('Enviando formulario...')
-		login( this.email.value , this.password.value)
-			.catch(error => this.setState( this.setErrorMessage('Usuario o Password incorrectos')))
+		login(this.email.value, this.password.value)
+			.catch(error => this.setState(this.setErrorMessage('Usuario o Password incorrectos')))
 	}
 
-	setErrorMessage(err){
+	setErrorMessage(err) {
 		return { loginMessage: err }
 	}
-	
+
 
 	render() {
 		return (
 			<article className='Main-container'>
 				<h1>Seccion de login</h1>
 				<form onSubmit={this.handleOnSubmit}>
-					<input type='email' placeholder='Email' ref={ email => this.email = email} />
-					<input type='password' placeholder='Password' ref={ password => this.password = password} />
+					<input type='email' placeholder='Email' ref={email => this.email = email} />
+					<input type='password' placeholder='Password' ref={password => this.password = password} />
 					{
 						this.state.loginMessage && //operador de cortocircuito
-						<div className='erro'>						
+						<div className='erro'>
 							<p>Error: {this.state.loginMessage}</p>
 						</div>
 					}
-					<input type='submit' value='Login' />					
+					<input type='submit' value='Login' />
 				</form>
 			</article>
 		)
--- 03-autenticacion-firebase/src/routes/index.jsx
+++ 03-autenticacion-firebase/src/routes/index.jsx
@@ -1,89 +1,124 @@
 //Deoendencies
-import React, { Component } from 'react'
+import React, { Component } from "react";
 import {
-	BrowserRouter,	//Invocar el manejo de rutas
-	Route,					//Manejo de rutas
-	Link,						//Manejo de enlaces
-	Redirect,				//Manejo de redirecciones
-	withRouter,			//Switch para modificar el valor de una ruta
-	Switch
-} from 'react-router-dom'
-import { firebaseAuth } from '../data/config'
+  BrowserRouter, //Invocar el manejo de rutas
+  Route, //Manejo de rutas
+  Link, //Manejo de enlaces
+  Redirect, //Manejo de redirecciones
+  withRouter, //Switch para modificar el valor de una ruta
+  Switch
+} from "react-router-dom";
+import { firebaseAuth } from "../data/config";
 //Assets
-import 'pure-css'
-import './css/index.css'
-import EDteamLogo from '../images/edteam-logo.png'
+import "pure-css";
+import "./css/index.css";
+import EDteamLogo from "../images/edteam-logo.png";
 //Components
-import Home from '../pages/'
-import About from '../pages/About'
-import Error404 from '../pages/Error404'
-import Login from '../pages/Login'
-import Register from '../pages/Register'
-import Protegida from '../pages/protected'
+import Home from "../pages/";
+import About from "../pages/About";
+import Error404 from "../pages/Error404";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import Protegida from "../pages/protected";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
 
-	constructor(props) {
-		super(props)
-	
-		this.state = {
-			 authed: false,
-			 loading: true
-		}
+    this.state = {
+      authed: false,
+      loading: true
+    };
 
-		this.handleOnClick = this.handleOnClick.bind(this)
-	}
+    this.handleOnClick = this.handleOnClick.bind(this);
+  }
 
-	handleOnClick() {
-		document.getElementById('tuckedMenu').classList.toggle('custom-menu-tucked');
-    document.getElementById('toggle').classList.toggle('x');
-	}
-	
+  handleOnClick() {
+    document
+      .getElementById("tuckedMenu")
+      .classList.toggle("custom-menu-tucked");
+    document.getElementById("toggle").classList.toggle("x");
+  }
 
-	render() {
-		return this.state.loading === false
-		? <h1>Cargando...</h1>
-		:	(
-			<BrowserRouter>
-				<div>
-					<header className="custom-menu-wrapper">
-						<div className="pure-menu custom-menu custom-menu-top">
-							<a href="#" className="pure-menu-heading custom-menu-brand">
-								<img src={EDteamLogo} alt='EDteam'/>
-							</a>
-							<a href="#" className="custom-menu-toggle" id="toggle" onClick={this.handleOnClick}><s className="bar"></s><s className="bar"></s></a>
-						</div>
-						<div className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked" id="tuckedMenu">
-								<div className="custom-menu-screen"></div>
-								<ul className="pure-menu-list">
-									<li className="pure-menu-item">
-										<Link to='/' className="pure-menu-link" onClick={this.handleOnClick}>Home</Link>
-									</li>
-									<li className="pure-menu-item">
-										<Link to='/acerca' className="pure-menu-link" onClick={this.handleOnClick}>Acerca</Link>
-									</li>
-									<li className="pure-menu-item">
-										<Link to='/registro' className="pure-menu-link" onClick={this.handleOnClick}>Registro</Link>
-									</li>
-									<li className="pure-menu-item">
-										<Link to='/login' className="pure-menu-link" onClick={this.handleOnClick}>Login</Link>
-									</li>
-								</ul>
-						</div>
-					</header>
-					<main className='Main'>
-						<Switch>
-							<Route path='/' exact component={Home}/>
-							<Route path='/acerca' component={About}/>
-							<Route path='/registro' component={Register}/>
-							<Route path='/login' component={Login}/>
-							<Route component={Error404} />
-						</Switch>
-					</main>
-				</div>
-			</BrowserRouter>
-		)
-	}
+  render() {
+    return this.state.loading === false ? (
+      <h1>Cargando...</h1>
+    ) : (
+      <BrowserRouter>
+        <div>
+          <header className="custom-menu-wrapper">
+            <div className="pure-menu custom-menu custom-menu-top">
+              <a href="#" className="pure-menu-heading custom-menu-brand">
+                <img src={EDteamLogo} alt="EDteam" />
+              </a>
+              <a
+                href="#"
+                className="custom-menu-toggle"
+                id="toggle"
+                onClick={this.handleOnClick}
+              >
+                <s className="bar" />
+                <s className="bar" />
+              </a>
+            </div>
+            <div
+              className="pure-menu pure-menu-horizontal pure-menu-scrollable custom-menu custom-menu-bottom custom-menu-tucked"
+              id="tuckedMenu"
+            >
+              <div className="custom-menu-screen" />
+              <ul className="pure-menu-list">
+                <li className="pure-menu-item">
+                  <Link
+                    to="/"
+                    className="pure-menu-link"
+                    onClick={this.handleOnClick}
+                  >
+                    Home
+                  </Link>
+                </li>
+                <li className="pure-menu-item">
+                  <Link
+                    to="/acerca"
+                    className="pure-menu-link"
+                    onClick={this.handleOnClick}
+                  >
+                    Acerca
+                  </Link>
+                </li>
+                <li className="pure-menu-item">
+                  <Link
+                    to="/registro"
+                    className="pure-menu-link"
+                    onClick={this.handleOnClick}
+                  >
+                    Registro
+                  </Link>
+                </li>
+                <li className="pure-menu-item">
+                  <Link
+                    to="/login"
+                    className="pure-menu-link"
+                    onClick={this.handleOnClick}
+                  >
+                    Login
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          </header>
+          <main className="Main">
+            <Switch>
+              <Route path="/" exact component={Home} />
+              <Route path="/acerca" component={About} />
+              <Route path="/registro" component={Register} />
+              <Route path="/login" component={Login} />
+              <Route component={Error404} />
+            </Switch>
+          </main>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
-export default App
+export default App;
