31336bd2314fb37c00ea395336409e79d0640273
JuanD1 <juandacor@outlook.com>
2018-06-28 23:14:14 -0500

    -

--- edcursos-react/src/pages/protected/DashboardCourses.jsx
+++ edcursos-react/src/pages/protected/DashboardCourses.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
+import App from "../../components/App";
 
 class Protegida extends Component {
   render() {
     return (
       <article className="Main-container">
         <h1>Seccion Protegida</h1>
+        <App />
       </article>
     );
   }
